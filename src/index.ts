92093c8a3516d140b9b375b26c78f1957035f43a
Pavel Gonzales <develop.gonzales@gmail.com>
2018-02-13 16:29:30 +0300

    add export perfect-scroll module

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,8 @@
 export { NgSelectComponent, NG_SELECT_DEFAULT_CONFIG } from './ng-select/ng-select.component';
 export { NgSelectModule } from './ng-select/ng-select.module';
 export { NgOption, NgSelectConfig } from './ng-select/ng-select.types';
+
+export { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
+export { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
+export { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
+
