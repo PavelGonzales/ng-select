ffd62401ac382b127d29f51a9bef024d05831ccb
Pavel Gonzales <develop.gonzales@gmail.com>
2018-02-13 17:37:47 +0300

    remove exports from index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,4 @@ export { NgSelectComponent, NG_SELECT_DEFAULT_CONFIG } from './ng-select/ng-sele
 export { NgSelectModule } from './ng-select/ng-select.module';
 export { NgOption, NgSelectConfig } from './ng-select/ng-select.types';
 
-export { PerfectScrollbarModule } from 'ngx-perfect-scrollbar';
-export { PERFECT_SCROLLBAR_CONFIG } from 'ngx-perfect-scrollbar';
-export { PerfectScrollbarConfigInterface } from 'ngx-perfect-scrollbar';
 
